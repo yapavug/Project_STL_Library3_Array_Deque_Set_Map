91d09b3aa3055172cdfdfcba3a0cd3c4a2599c17
yapavug <pavelguliaev2018@mail.ru>
2025-01-09 16:54:16 +0300

    Добавлено рассмотрение контекнеров map и multimap

--- Source.cpp
+++ Source.cpp
@@ -3,6 +3,8 @@ using namespace std;
 #include <array>
 #include <deque>
 #include <set>
+#include <map>
+#include <string>
 
 int main()
 {
@@ -42,7 +44,7 @@ int main()
 
 
 
-	cout << "1. ---		DEQUE		---" << endl << endl;
+	cout << "2. ---		DEQUE		---" << endl << endl;
 	//Представляет собой реализацию двусторонней очереди - double-ended queue: list + vector - "скрещение динамического массива и двусвязного списка"
 
 
@@ -50,7 +52,7 @@ int main()
 	//Все методы уже рассмотрены при работе с предыдущими контейнерами list и vector
 	
 
-	cout << "2. ---		SET И MULTISET		---" << endl << endl;
+	cout << "3. ---		SET И MULTISET		---" << endl << endl;
 	// Это упорядоченные ассоциативные контейнеры, основаны на принципе бинарного дерева
 
 	set<int> mySet;
@@ -98,6 +100,51 @@ int main()
 	auto it2 = ms.lower_bound(1); // Возвращает первый аргумент в коллекции
 
 	auto a = ms.equal_range(2); // Возвращает соседние lower_bound и upper_bound 
+	\
+
+
+
+
+
+	cout << endl;
+
+
+	cout << "4. ---		PAIR		---" << endl << endl;
+	pair<int, string> p(1,"телефон"); // Пара - позволяет связать ключ и значение
+	
+	cout << p.first << endl;
+	cout << p.second << endl;
+
+
+	cout << "5. ---		MAP И MULTIMAP		---" << endl << endl;
+	//Похоже на set, но хранят пары значений ключ-значение
+	//Банирное дерево строится по ключу
+
+	map<int, string> myMap;
+	myMap.insert(make_pair(1, "ноутбук")); //Создает объект pair и перемещает в map
+	myMap.insert( pair<int,string>(2, "телефон")); //Создает объект pair и перемещает в map
+
+	myMap.emplace(3, "монитор"); //Создает pair сразу в map
+
+	myMap.emplace(44, "клавиатура");
+
+	auto itr = myMap.find(66); // Метод find работает точно так же как и для set
+
+	auto itr2 = myMap.emplace(44, "мышка"); // Таким образом можно контролировать, удалось ли добавить элемент
+
+	cout << myMap[44] << endl;
+
+	myMap.erase(2); // Удаление эл-та
+
+	// [] можно добавить эл-т или изменить значение для существующего
+	//Метод at - можно изменить значение для существующего, если эл-та нет - вызовется исключение
+
+	multimap<int, string> mp;
+	mp.emplace(1, "Петя");
+	mp.emplace(1, "Вася");
+	mp.emplace(3, "Маша");
+
+	//Для multimap отсутствуют оператор [] и метод at
 
 	return 0;
 }
